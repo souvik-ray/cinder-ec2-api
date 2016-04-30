d47c875561b77943bb7f14327ead0b6984d091df
souvik <souvik.ray@ril.com>
2016-04-30 16:40:55 +0530

    Fix Metrics Wrapper

--- ec2api/api/ec2api_metricutil.py
+++ ec2api/api/ec2api_metricutil.py
@@ -1,5 +1,5 @@
-from utils.metricutil import metricutil
+from metrics.metricutil import metricutil
 # Wrapper for Cinder Volume
 class Ec2APIMetricsWrapper(SyncFlowMetricsWrapper):
     def __init__(self, operation_name):
-        super(CinderVolumeMetricsWrapper, self).__init__("Ec2API", "/var/log/ec2api/service.log")
+        super(Ec2APIMetricsWrapper, self).__init__("Ec2API", "/var/log/ec2api/service.log")
